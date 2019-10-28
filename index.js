9b9a1171cded1ada6c1cd120eab9f45ec296dbf1
VBTX <jv.svedaite@gmail.com>
2019-10-28 13:20:56 -0500

    add logger to test events module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const person = require('./person');
+const Logger = require('./logger');
 
 const Cat = require('./cat');
 
@@ -6,4 +7,8 @@ console.log(person);
 
 const myKitty = new Cat('Priscilla', 2);
 
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener', data));
+logger.log('Hi there');
 myKitty.greeting();
