d8b180476dc3cedf50c518aa4a293b392bb08b21
VBTX <jv.svedaite@gmail.com>
2019-10-28 14:00:43 -0500

    practice building end points and serving html or json

--- index.js
+++ index.js
@@ -3,12 +3,59 @@ const Logger = require('./logger');
 
 const Cat = require('./cat');
 
-console.log(person);
+// console.log(person);
 
-const myKitty = new Cat('Priscilla', 2);
+// const myKitty = new Cat('Priscilla', 2);
 
-const logger = new Logger();
+// const logger = new Logger();
 
-logger.on('message', data => console.log('Called Listener', data));
-logger.log('Hi there');
-myKitty.greeting();
+// logger.on('message', data => console.log('Called Listener', data));
+// logger.log('Hi there');
+// myKitty.greeting();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+	// console.log(req.url); to see where the request is coming from
+	if (req.url === '/') {
+		fs.readFile(
+			path.join(__dirname, 'public', 'index.html'),
+			(err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.end(content);
+			}
+		);
+	}
+
+	if (req.url === '/about') {
+		fs.readFile(
+			path.join(__dirname, 'public', 'about.html'),
+			(err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.end(content);
+			}
+		);
+	}
+
+	if (req.url === '/api/users') {
+		//example for json REST
+
+		const users = [
+			{ name: 'Victoria', location: 'Dallas, TX' },
+			{ name: 'John', location: 'Austin, TX' }
+		];
+		fs.readFile(
+			path.join(__dirname, 'public', 'about.html'),
+			(err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				res.end(JSON.stringify(users)); // turning JS array of objects to json
+			}
+		);
+	}
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ reference/http_demo.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+// Create a server object
+
+// http
+// 	.createServer((req, res) => {
+// 		//Write a response to a reqyuest
+// 		res.write('Hello there');
+// 		res.end();
+// 	})
+// 	.listen(5000, () => console.log('Server is running'));
